67f7319779eedfc1ec869fd3941c069ad111c65e
David Segura <david@segura.org>
2019-03-26 23:46:39 -0500

    19.3.26.0

--- OSDSoftware.psd1
+++ OSDSoftware.psd1
@@ -7,7 +7,7 @@
 RootModule = 'OSDSoftware.psm1'
 
 # Version number of his module.
-ModuleVersion = '19.2.19.0'
+ModuleVersion = '19.3.26.0'
 
 # Supported PSEditions
 # CompatiblePSEditions = @()
--- OSDSoftware.psm1
+++ OSDSoftware.psm1
@@ -1,11 +1,14 @@
-$Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
-$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+#===================================================================================================
+#   Import Functions
+#   https://github.com/RamblingCookieMonster/PSStackExchange/blob/master/PSStackExchange/PSStackExchange.psm1
+#===================================================================================================
+$OSDPublicFunctions  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+$OSDPrivateFunctions = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 
-foreach ($Import in @($Public + $Private)) {
+foreach ($Import in @($OSDPublicFunctions + $OSDPrivateFunctions)) {
     Try {. $Import.FullName}
     Catch {Write-Error -Message "Failed to import function $($Import.FullName): $_"}
 }
 
-Export-ModuleMember -Function $Public.BaseName
-
-#https://github.com/RamblingCookieMonster/PSStackExchange/blob/master/PSStackExchange/PSStackExchange.psm1
+Export-ModuleMember -Function $OSDPublicFunctions.BaseName
+#===================================================================================================
